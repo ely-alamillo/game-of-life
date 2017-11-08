d9c585cb9589e004ce2b1ab9422c9b22bad016c7
Ely Alamillo <ely0013@gmail.com>
2017-11-08 00:26:38 -0600

    added get route for heroku

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const server = express();
 
 server.use(express.static(path.join(__dirname, 'build')));
 
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
+
 server.listen(process.env.PORT || 8080, () => {
   console.log('I am now listen on the available port');
 });
